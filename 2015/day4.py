42479991459214c0b5dc58af84897b9599edfad2
sschlingmann04 <sschlingmann@ufl.edu>
2026-01-05 23:53:34 -0500

    completed 2015 day 4

---
+++ 2015/day4.py
@@ -0,0 +1,24 @@
+# Day 4: The Ideal Stocking Stuffer
+
+from hashlib import md5
+
+# Part 1
+def findSecretNumber(key: str, match: str):
+    num = 1
+    while True:
+        res = md5((key + str(num)).encode())
+        if res.hexdigest().startswith(match):
+            return num
+        num += 1
+
+
+# Example main program
+if __name__ == "__main__":
+    try:
+        with open("2015/input_files/day4.txt", "r") as f:
+            key = f.read()
+    except FileNotFoundError:
+        print(f"Error: File was not found!")
+    
+    print(f"The lowest positive number that produces a hash that starts with 5 zeroes is {findSecretNumber(key, "00000")}.")
+    print(f"The lowest positive number that produces a hash that starts with 6 zeroes is {findSecretNumber(key, "000000")}.")
